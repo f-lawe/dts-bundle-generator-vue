cb3c7e4e55f6e11ad5fc9ff92f0c7aa118b36002
Flawe <francois@lavaud.family>
2023-03-30 16:55:57 +0200

    Use reference

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./shims.d.ts" />
+
 export const Foo = 'bar';
 
 export { default as MyComponent } from './MyComponent.vue';
